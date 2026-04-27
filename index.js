939aa68349d50ea2531a5a4adf72d99bd2712f97
albertquintana429-cmyk <albertquintana429@gmail.com>
2026-04-27 18:01:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 console.log("TOKEN:", process.env.TOKEN);
 
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const fs = require("fs");
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessages
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
   ],
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
+// colección de comandos
+client.slashCommands = new Collection();
+
+// cargar comandos
+const commandFiles = fs.readdirSync("./Comandos").filter(f => f.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./Comandos/${file}`);
+  client.slashCommands.set(command.name, command);
+}
+
+// ejecutar comandos
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.slashCommands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    interaction.reply({ content: "❌ Error en el comando", ephemeral: true });
+  }
+});
+
+// ready
 client.on('ready', () => {
   console.log(`🔥 Estoy online como ${client.user.tag}`);
 });
