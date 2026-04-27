567401e583b9259f5d6ad70212d07e50dbe7a82b
albertquintana429-cmyk <albertquintana429@gmail.com>
2026-04-27 18:07:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,22 @@ const client = new Client({
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
-// colección de comandos
+// comandos
 client.slashCommands = new Collection();
 
-// cargar comandos
-const commandFiles = fs.readdirSync("./Comandos").filter(f => f.endsWith(".js"));
+// cargar comandos de subcarpetas
+const folders = fs.readdirSync("./Comandos");
 
-for (const file of commandFiles) {
-  const command = require(`./Comandos/${file}`);
-  client.slashCommands.set(command.name, command);
+for (const folder of folders) {
+  const files = fs.readdirSync(`./Comandos/${folder}`).filter(f => f.endsWith(".js"));
+
+  for (const file of files) {
+    const command = require(`./Comandos/${folder}/${file}`);
+    if (command.name) client.slashCommands.set(command.name, command);
+  }
 }
 
-// ejecutar comandos
+// interacción comandos
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
@@ -44,4 +48,5 @@ client.on('ready', () => {
   console.log(`🔥 Estoy online como ${client.user.tag}`);
 });
 
+// login
 client.login(process.env.TOKEN);
