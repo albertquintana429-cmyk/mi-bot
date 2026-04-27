66f354b6229a1a93dabcb177b70779eeecaff50c
albertquintana429-cmyk <albertquintana429@gmail.com>
2026-04-27 19:14:45 -0400

    Update predator.js

--- Comandos/entregas/predator.js
+++ Comandos/entregas/predator.js
@@ -1,39 +1,41 @@
-const Discord = require("discord.js");
-const config = require('../../config.json');
+const { ApplicationCommandType, ApplicationCommandOptionType, EmbedBuilder } = require("discord.js");
+
+const config = {
+  eventas: process.env.EVENTAS,
+  colorpredeterminado: process.env.COLOR,
+  feedback: process.env.FEEDBACK
+};
 
 module.exports = {
-  name: "predator", // Nombre del comando
-  description: "📦​ | Entrega Predator", // Descripción
-  type: Discord.ApplicationCommandType.ChatInput,
+  name: "predator",
+  description: "📦​ | Entrega Predator",
+  type: ApplicationCommandType.ChatInput,
   options: [
     {
       name: "key",
       description: "Ingrese la/s key(s).",
-      type: Discord.ApplicationCommandOptionType.String,
+      type: ApplicationCommandOptionType.String,
       required: true,
     }
   ],
 
-  run: async (client, interaction) => {
-    // Verificar si el usuario tiene el rol requerido
+  execute: async (interaction) => {
     const requiredRoleId = `${config.eventas}`;
     const member = interaction.member;
     const hasRole = member.roles.cache.has(requiredRoleId);
 
     if (!hasRole) {
-      return interaction.reply({ 
-        content: "<:warninghost:1383935369275379874> | No tienes permiso para usar este comando.", 
-        ephemeral: true 
+      return interaction.reply({
+        content: "<:warninghost:1383935369275379874> | No tienes permiso para usar este comando.",
+        ephemeral: true
       });
     }
 
-    // Datos
-    const bot = client.user.username;
-    const avatar_bot = client.user.displayAvatarURL({ dynamic: true });
+    const bot = interaction.client.user.username;
+    const avatar_bot = interaction.client.user.displayAvatarURL();
     const key = interaction.options.getString("key");
 
-    // Embed de entrega
-    const embed = new Discord.EmbedBuilder()
+    const embed = new EmbedBuilder()
       .setTitle("¡Gracias por tu compra! 🎉")
       .setColor(config.colorpredeterminado)
       .setTimestamp()
@@ -43,16 +45,14 @@ module.exports = {
         `**•  __Producto__:** Predator CS2 <:predator:1411596003718594623>\n\n` +
         `**•  Key(s):** ||${key}||\n` +
         `**•  Website:** [Haz Click Aqui](https://predator.systems/)\n\n` +
-        `Déjanos por favor un ${config.feedback} para poder seguir creciendo! <a:blackverify:1360058374456348846><:coramanos:1387181348069838942>`
+        `Déjanos por favor un ${config.feedback} para poder seguir creciendo!`
       );
 
-    // 1. Enviar mensaje ephemeral al usuario
     await interaction.reply({
       content: "✅ Producto entregado exitosamente.",
       ephemeral: true
     });
 
-    // 2. Enviar embed públicamente al canal
     await interaction.channel.send({ embeds: [embed] });
   }
-}
+};
